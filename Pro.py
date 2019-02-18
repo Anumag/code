742563ebf8cf7a7b2c290caae568f89f552b8805
Anumag <47672001+Anumag@users.noreply.github.com>
2019-02-19 00:00:46 +0530

    Create Pro.py

---
+++ Pro.py
@@ -0,0 +1,14 @@
+a=input()
+g=""
+f=[]
+for i in range(0,len(a)):
+    if a[i] not in g:
+        g=g+a[i]
+    elif a[i] in g :
+        f.append(len(g))
+        g=""
+    if i==len(a)-1:
+        f.append(len(g))
+        g=""
+    
+print(max(f))
